04ff3b04c868517c04ded4093bfa36f38d3860ba
IgorPaslauski <igor.oliveira@abase.com.br>
2025-02-14 10:25:24 -0300

    Reiniciado

--- src/app/Main/MainConteudo/MainConteudoTitle.tsx
+++
@@ -1,3 +0,0 @@
-export function MainConteudoTitle({ title }: { title: string }) {
-  return <h1 className="text-3xl font-bold">{title}</h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Main } from "./Main/Main";
-import { Nav } from "./Nav/Nav";
+
 
 export default function Home() {
   return (
     <>
-      <div className="flex">
-        <Nav />
-        <Main />
-      </div>
+      <h1>Home</h1>
     </>
   );
 }
